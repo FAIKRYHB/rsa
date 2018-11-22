d06939431bd4b7965fab0cb55b66267d48e652ce
Ondřej Brabec <ondrej.brabec@dgstudio.cz>
2018-11-22 15:40:57 +0100

    Zadání

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1. p,q => prvočísla 1*10^16.... 9.9....
+# 2. n = p*q
+# 3. Fí(n) = (p-1)*(q-1)
+# 4. 1 < e < Fi(n) : e X Fi(n)  GCD(e, Fí(n)) == 1
+# 5. inverzní modulo e : d => e mod Fí(n) = 1
+## Šifrování
+# OT = 'Ahoj pepo'
+# Št = OT^e * mod n
+# Ze znaku bin číslo a převést na jedno decimální po 7 znacích
+
+## Dešifrování
+# OT = ŠT^d * mod n
+
+# Výstup bude soubor bloků čísel.
+
