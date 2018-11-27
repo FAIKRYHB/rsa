132ec496f7218369d8544226a94e08067c917b87
Jan Hošek <j_hosek@utb.cz>
2018-11-27 19:26:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,40 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 class rsa(QMainWindow, Ui_MainWindow):
     
+    mode=True #true sifrovat
+    
+    def switchmode(self):
+        if self.mode:
+            self.SifDesif.setText("Dešifrovat")
+            self.label_InKey.setText("Soukromý klíč")
+            self.label_InDE.setText("d")
+            self.label_OutDE.setText("e")
+            self.randomKey.setEnabled(False)
+            self.mode=False
+        else:
+            self.SifDesif.setText("Šifrovat")
+            self.label_OutKey.setText("Veřejný klíč")
+            self.label_InDE.setText("e")
+            self.label_OutDE.setText("d")
+            self.randomKey.setEnabled(True)
+            self.mode=True
+    
+    def run(self):
+        if self.mode:
+            self.sifrovat
+        else:
+            self.desifrovat
+    
+    
     def __init__(self):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         
         self.setupUi(self)
         
-        self.codeButton.clicked.connect(self.code)
-        self.decodeButton.clicked.connect(self.decode)
+        self.SifDesif.clicked.connect(self.run)
+        self.SwitchSifDes.clicked.connect(self.switchmode)
+        self.randomKey.clicked.connect(self.randomisekey)
     
         self.statusBar = QStatusBar()
         self.setStatusBar(self.statusBar)
